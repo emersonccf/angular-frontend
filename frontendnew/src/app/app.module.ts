7724b17d9b893b8160433e0c37c74f13fcff3c0c
Emerson Ferreira <emecatarino@yahoo.com.br>
2022-07-15 00:05:24 -0300

    Inicio pagina Home

--- frontendnew/src/app/app.module.ts
+++ frontendnew/src/app/app.module.ts
@@ -8,16 +8,19 @@ import { HeaderComponent } from './components/template/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatCardModule } from '@angular/material/card'
 import { MatListModule } from '@angular/material/list'
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +29,7 @@ import { NavComponent } from './components/template/nav/nav.component';
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
