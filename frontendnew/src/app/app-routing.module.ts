6ae7360c13bb71939f996db093aef82c185b81c2
Emerson Ferreira <emecatarino@yahoo.com.br>
2022-07-19 21:03:55 -0300

    Navegando entre componentes

--- frontendnew/src/app/app-routing.module.ts
+++ frontendnew/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
-const routes: Routes = [];
+const appName: string = "MyCRUD" // poderia colocar esta constante em um service para utilizar em toda a aplicação
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+    title: appName + " | " + "Home",
+  },
+  {
+    path: "products",
+    component: ProductCrudComponent,
+    title: appName + " | " + "Produtos",
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
