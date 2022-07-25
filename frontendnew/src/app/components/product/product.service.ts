e67c39576892ef35798fe057aeb29104f57698aa
Emerson Ferreira <emecatarino@yahoo.com.br>
2022-07-24 23:13:18 -0300

    Implementacao de tratamento de erro

--- frontendnew/src/app/components/product/product.service.ts
+++ frontendnew/src/app/components/product/product.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { MatSnackBar } from '@angular/material/snack-bar'
-import { Observable } from 'rxjs'
+import { EMPTY, Observable } from 'rxjs'
+import { map, catchError } from 'rxjs/operators'
 import { ProductReadComponent } from './product-read/product-read.component'
 import { Product } from './product.model'
 @Injectable({
@@ -35,26 +36,52 @@ export class ProductService {
     this._productReadComponent = productReadComponent
   }
   createProduct(product: Product): Observable<Product> {
-    return this.httpClient.post<Product>(this.baseUrl, product)
+    return this.httpClient.post<Product>(this.baseUrl, product).pipe(
+      map(obj => obj),
+      catchError(error => this.handleError(error))
+    )
   }
 
   readProducts(): Observable<Product[]> {
-    return this.httpClient.get<Product[]>(this.baseUrl)
+    return this.httpClient.get<Product[]>(this.baseUrl).pipe(
+      map(obj => obj),
+      catchError(error => this.handleError(error))
+    )
   }
 
   readByIdProduct(id: Number): Observable<Product> {
     const url = `${this.baseUrl}/${id}`
-    return this.httpClient.get<Product>(url)
+    return this.httpClient.get<Product>(url).pipe(
+      map(obj => obj),
+      catchError(error => this.handleError(error))
+    )
   }
 
   updateProduct(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product.id}`
-    return this.httpClient.put<Product>(url, product)
+    return this.httpClient.put<Product>(url, product).pipe(
+      map(obj => obj),
+      catchError(error => this.handleError(error))
+    )
   }
 
   deleteProduct(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product.id}`
-    return this.httpClient.delete<Product>(url)
+    return this.httpClient.delete<Product>(url).pipe(
+      map(obj => obj),
+      catchError(error => this.handleError(error))
+    )
   }
 
+  // Source: https://www.positronx.io/angular-error-handling-tutorial-with-examples/
+  handleError(error: any): Observable<any> {
+    let errorMessage = ''
+    if (error.error instanceof ErrorEvent) { //clint-side error
+      errorMessage = `Erro clint-side: ${error.error.message}`
+    } else { // server-side error
+      errorMessage = `Erro server-side: ${error.status} --> Message: ${error.message}`
+    }
+    this.showMessage(errorMessage, ['danger', 'font-white'])
+    return EMPTY
+  }
 }
